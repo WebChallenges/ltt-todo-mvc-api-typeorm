018e650d6564b8fa88827d31987329cdb3216ae6
Tien Nguyen Dang <tiennd@pop-os.localdomain>
2020-11-05 15:58:27 +0700

    Modify database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HttpErrorFilter } from './shared/http-error.filter';
@@ -10,19 +9,11 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env.local',
-    }),
     TypeOrmModule.forRoot(
       process.env.NODE_ENV === 'production'
         ? {
             type: 'postgres',
-            host: 'ec2-54-82-208-124.compute-1.amazonaws.com',
-            port: 5432,
-            username: 'rdargqofsuksac',
-            password:
-              '6dfcb866ee18f7159c340c44e9b2bffd6bb1d5f5fb1b898113e8f22ce91eb99c',
-            database: 'dcblogopp8k04b',
+            url: process.env.DATABASE_URL,
             entities: ['dist/**/*.entity.js'],
             synchronize: true,
           }
